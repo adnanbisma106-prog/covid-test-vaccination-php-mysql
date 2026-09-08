659432b08d52c451d6e33ed4a356c42c33913425
adnanbisma106-prog <adnanbisma106@gmail.com>
2026-09-09 02:20:20 +0500

    Add includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Global Footer Component
+ */
+?>
+
+<!-- Bootstrap 5 Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Chart.js -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+</body>
+</html>
